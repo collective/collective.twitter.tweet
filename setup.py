b955634108884dcceb33e929fcb1902f70b7dd96
Franco Pellegrini <frapell@gmail.com>
2012-10-07 14:15:54 +0200

    Preparing release 1.0b3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0b3.dev0'
+version = '1.0b3'
 long_description = open("README.txt").read() + "\n" + \
                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
                    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
