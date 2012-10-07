f5ca7e2d16fdc8b7d25d6b2acafa347d424c6e5e
Franco Pellegrini <frapell@gmail.com>
2012-10-07 14:16:36 +0200

    Back to development: 1.0b4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0b3'
+version = '1.0b4.dev0'
 long_description = open("README.txt").read() + "\n" + \
                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
                    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
